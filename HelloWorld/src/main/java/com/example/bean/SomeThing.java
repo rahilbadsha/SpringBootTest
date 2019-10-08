fe36f3ee62cc55290ba02ce7f373c784fa1d6ca7
rahil <rahil@DESKTOP-1GKQH1C>
2019-10-08 14:34:02 +0530

    changed
    Change-Id: I89e78800d5dd24edf24fb7fe0538eede08e31125

--- HelloWorld/src/main/java/com/example/bean/SomeThing.java
+++ HelloWorld/src/main/java/com/example/bean/SomeThing.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component("SomeThing")
 public class SomeThing {
 	
+	
 	public void someLogic() {
 		System.out.println("Git hub webhook test");
 		System.out.println("another Git hub webhook test");
